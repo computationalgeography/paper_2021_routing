b20b7c9a561a703e26d9f138f43dc4b730580db7
Kor de Jong <k.dejong1@uu.nl>
2021-04-12 15:53:06 +0200

    WIP

--- source/accumulate_flow.sh
+++ source/accumulate_flow.sh
@@ -3,8 +3,10 @@ set -e
 
 lue_translate="$LUE_OBJECTS/bin/lue_translate"
 
-input_dataset_pathname="/mnt/data1/home/kor/data/project/routing/dem_ldd/108680/ldd_108680.lue"
-flow_direction_array_pathname="area/raster/ldd_108680"
+area="pyrenees"
+
+# input_dataset_pathname="/mnt/data1/home/kor/data/project/routing/dem_ldd/108680/ldd_108680.lue"
+# flow_direction_array_pathname="area/raster/ldd_108680"
 
 # input_dataset_pathname="/mnt/data1/home/kor/data/project/routing/africa/africa.lue"
 # flow_direction_array_pathname="area/raster/flow_direction"
@@ -12,12 +14,16 @@ flow_direction_array_pathname="area/raster/ldd_108680"
 # input_dataset_pathname="/mnt/data1/home/kor/data/project/routing/africa2/ldd_africa.lue"
 # flow_direction_array_pathname="area/raster/ldd"
 
-output_prefix="/tmp"
+input_dataset_pathname="$LUE_ROUTING_DATA/$area/ldd_108680.lue"
+flow_direction_array_pathname="area/raster/ldd_108680"
+
+output_prefix="$LUE_ROUTING_DATA/tmp"
 output_dataset_pathname="$output_prefix/tmp.lue"
 
 
 # NOTE: using multiple localities requires a build of LUE with support for
 #     parallel I/O
+
 nr_localities=1
 nr_threads_per_locality=4
 
@@ -25,11 +31,39 @@ PYTHONPATH=$LUE/../paper_2021_routing/source/:$PYTHONPATH \
     `which python` $LUE_OBJECTS/_deps/hpx-build/bin/hpxrun.py \
         --localities=$nr_localities \
         --thread=$nr_threads_per_locality \
-        --parcelport=tcp \
+        --runwrapper=mpi \
+        --parcelport=mpi \
         --verbose \
         `which python` -- $LUE/../paper_2021_routing/source/accumulate_flow.py \
             $input_dataset_pathname $flow_direction_array_pathname \
             $output_dataset_pathname
+
+
+### # Fixed. Depends on platform.
+### nr_numa_domains_per_node=8
+### nr_cores_per_socket=6
+### nr_cpus_per_task=12
+### cpu_binding="thread:0-5=core:0-5.pu:0"
+### 
+### # Depends on size of job
+### nr_nodes=2
+### 
+### # Fixed
+### nr_tasks=$(expr $nr_nodes \* $nr_numa_domains_per_node)
+### 
+### 
+### srun \
+###     --ntasks $nr_tasks \
+###     --cpus-per-task=$nr_cpus_per_task \
+###     --cores-per-socket=$nr_cores_per_socket \
+###     --kill-on-bad-exit \
+###     python $LUE/../paper_2021_routing/source/accumulate_flow.py \
+###         $input_dataset_pathname $flow_direction_array_pathname \
+###         $output_dataset_pathname \
+###         --hpx:ini="hpx.parcel.mpi.enable=1" \
+###         --hpx:ini="hpx.os_threads=$nr_cores_per_socket" \
+###         --hpx:bind=$cpu_binding \
+###         --hpx:print-bind
 
 
 # Post-processing
